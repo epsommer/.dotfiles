ee1d8f8bff3ecc721488b2a67f3a190ecc8697f0
epsommer <admin@evangelosommer.com>
2024-07-26 20:44:34 -0400

    Update .gitignore to include kitty and fish directories

---
+++ .config/fish/config.fish
@@ -0,0 +1,50 @@
+# Fish shell configuration
+
+# ----------------------------
+# Check if the shell session is interactive
+# ----------------------------
+if status is-interactive
+    # ----------------------------
+    # Set the PATH for user-installed functions and binaries
+    # ----------------------------
+    set -g fish_user_paths ~/.config/fish/functions $fish_user_paths
+
+    # ----------------------------
+    # Check if Fisher is installed
+    # ----------------------------
+    if test -f ~/.config/fish/functions/fisher.fish
+        fisher --version
+    else
+        echo "Fisher is not installed"
+    end
+
+    # ----------------------------
+    # Configure Tide if it is installed
+    # ----------------------------
+    if functions -q tide
+        # Avoid running `tide configure` on every startup
+        if not test -f ~/.config/fish/conf.d/tide_configured.fish
+            tide configure
+            touch ~/.config/fish/conf.d/tide_configured.fish
+        end
+    else
+        echo "Tide is not installed"
+    end
+end
+
+# ----------------------------
+# Aliases
+# ----------------------------
+alias ls='eza --color=always'
+alias vim='nvim'
+alia tmux="tmux -f ~/.config/tmux/tmux.conf"
+
+# ----------------------------
+# Add Rust's Cargo binaries to PATH
+# ----------------------------
+set -gx PATH $HOME/.cargo/bin $PATH
+
+# ---------------------------
+# Use SecretService for Git credentials
+# ---------------------------
+set -x GCM_CREDENTIAL_STORE secretservice
